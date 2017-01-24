b300980accaf5a2d71f0b00b2f93261e635cae07
leileibill <leileibill@gmail.com>
2017-01-23 21:53:19 -0600

    Added 3level simulation and matlab plot

---
+++ Ltspice/Matlab/plot_balance_3level.m
@@ -0,0 +1,61 @@
+clc;
+close all;
+clear;
+set_figure_style_pre();
+
+%% Circuit parameter
+fsw = 120e3;
+Rload = [50 25 10 5 3 2 1];
+Vin = 50;
+duty = 0.25;
+current = Vin*duty./Rload;
+
+
+%% Calculate imablance
+
+figure;
+var_name = '3level_delay.mat';
+load(var_name);
+calculate_balance_3level;
+
+plot(current, v_diff,'s-')
+hold on;
+
+% var_name = '7level_delay_3delay_3delay2.mat';
+% load(var_name);
+% calculate_balance;
+% 
+% plot(current, v_diff,'o-')
+
+ylabel('Voltage difference [V]')
+xlabel('Load current [A]')
+legend({'delay = 3 ns, delay2 = 2 ns','delay = 3 ns, delay2 = 3 ns'},'Location','SouthEast')
+
+set_figure_style();
+resize_figure(2);
+% export_figure('plot_balance','png')
+
+return
+
+%% Calculate imablance, reverse order pwm
+figure;
+var_name = '7level_delay_reverse_3delay_2delay2.mat';
+load(var_name);
+calculate_balance;
+
+plot(current, v_diff,'s-')
+hold on;
+
+var_name = '7level_delay_reverse_3delay_3delay2.mat';
+load(var_name);
+calculate_balance;
+
+plot(current, v_diff,'s-')
+
+ylabel('Voltage difference [V]')
+xlabel('Load current [A]')
+legend({'delay = 3 ns, delay2 = 2 ns','delay = 3 ns, delay2 = 3 ns'},'Location','SouthEast')
+
+set_figure_style();
+resize_figure(2);
+% export_figure('plot_balance_reverse','png')
