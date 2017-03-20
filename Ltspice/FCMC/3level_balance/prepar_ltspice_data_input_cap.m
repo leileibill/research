e80b0e34fe469fa65c60a99450be82dbfc77a419
leileibill <leileibill@gmail.com>
2017-03-20 10:43:13 -0500

    Modified the balance simulation file

--- Ltspice/FCMC/3level_balance/prepar_ltspice_data_input_cap.m
+++ Ltspice/FCMC/3level_balance/prepar_ltspice_data_input_cap.m
@@ -1,5 +1,13 @@
 %%  only need to run for the first time
 clear;
-[iload vc]=read_Ltspice_data('3level_input_cap.log','vc1');
-[iload vin]=read_Ltspice_data('3level_input_cap.log','vin');
-save('3level_input_cap_L0.01u_C30u_duty0.75_0n_0n.mat');
+[cin vc]=read_Ltspice_data('3level_input_cap.log','vc1');
+[cin vin]=read_Ltspice_data('3level_input_cap.log','vin');
+condition = '3level_input_cap_Lin0.1u_Lout100u_Iload4_duty0.75_0n_0n_vs_cin'
+save('3leve_balance_vs_cin_Lout100u_duty0.75.mat');
+
+
+% clear;
+% [iload vc]=read_Ltspice_data('3level_input_cap.log','vc1');
+% [iload vin]=read_Ltspice_data('3level_input_cap.log','vin');
+% condition = '3level_input_cap_Lin0.1u_Lout100u_Iload4_duty0.75_0n_0n_vs_cin'
+% save('3leve_balance_vs_cin_Lout100u_duty0.75.mat');
